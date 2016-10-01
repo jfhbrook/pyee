532c80a61bb428fa9b2d73cfd227dc6e95a77c00
Joshua Holbrook <josh.holbrook@gmail.com>
2016-10-01 16:38:37 -0400

    Exclude asyncio tests from 3.4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
+from sys import version_info as v
+
 collect_ignore = []
 
-try:
-    import asyncio
-except ImportError:
+if not (v[0] >= 3 and v[1] >= 5):
     collect_ignore.append('test_asyncio.py')
