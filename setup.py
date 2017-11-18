7cacc0e07c3e61c2aa8250b2338e0af5338d11ee
Joshua Holbrook <josh.holbrook@gmail.com>
2017-11-17 15:14:10 -0900

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
         "Topic :: Other/Nonlisted Topic"
     ]
 )
