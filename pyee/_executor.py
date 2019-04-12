fdd538c2de02551cae2df7ae96ef826489d42dcf
Joshua Holbrook <josh.holbrook@gmail.com>
2019-04-12 19:33:21 -0400

    Update copy on executor docs

--- pyee/_executor.py
+++ pyee/_executor.py
@@ -19,22 +19,19 @@ class ExecutorEventEmitter(BaseEventEmitter):
     a custom executor may also be passed in explicitly to, for instance,
     use a ``ProcessPoolExecutor`` instead.
 
-    This class is a context manager, and can be used in ``with`` statements::
+    The underlying executor may be shut down by calling the ``shutdown``
+    method. Alternately you can treat the event emitter as a context manager:
 
         with ExecutorEventEmitter() as ee:
+            # Underlying executor open
+
             @ee.on('data')
-            def slow_data_handler(data):
-                sleep(0.5)
+            def handler(data):
                 print(data)
 
             ee.emit('event')
 
-            sleep(1)  # slow data handler will have printed
-
-    The behavior on exit is to shut down the underlying executor. Alternately,
-    create the event emitter as normal and manually call the ``shutdown``
-    method when done, or pass in an existing executor and manage the lifecycle
-    of that executor independently.
+        # Underlying executor closed
 
     No effort is made to ensure thread safety, beyond using an executor.
     """
