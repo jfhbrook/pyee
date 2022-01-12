5435442490bd6ad4289ae868479fb92129cd35a9
Josh Holbrook <josh.holbrook@gmail.com>
2022-01-11 23:57:00 -0500

    update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ author = "Josh Holbrook"
 # built documents.
 #
 # The short X.Y version.
-version = "8.2.2"
+version = "9.0.0"
 
 # The full version, including alpha/beta/rc tags.
 release = version
