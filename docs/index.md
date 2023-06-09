d639575a7578a0f15f80a2b2803f97d2983e17b6
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-08 18:57:11 -0800

    Backport template to project

---
+++ docs/index.md
@@ -0,0 +1,19 @@
+# Getting Started
+
+pyee is a rough port of
+[node.js's EventEmitter](https://nodejs.org/api/events.html). Unlike its
+namesake, it includes a number of subclasses useful for implementing async
+and threaded programming in python, such as async/await as seen in python 3.5+.
+
+## Install
+
+You can install this project into your environment of choice using `pip`:
+
+    pip install pyee
+
+## Some Links
+
+* [API Docs](./api.md)
+* [Changelog](./changelog.md)
+* [Fork Me On GitHub](https://github.com/jfhbrook/pyee)
+* [These Very Docs on readthedocs.io](https://pyee.rtfd.io)
--- pyee/__init__.py
+++ pyee/__init__.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """
-pyee supplies a ``EventEmitter`` class that is similar to the
-``EventEmitter`` class from Node.js. In addition, it supplies the subclasses
-``AsyncIOEventEmitter``, ``TwistedEventEmitter`` and ``ExecutorEventEmitter``
+pyee supplies a `EventEmitter` class that is similar to the
+`EventEmitter` class from Node.js. In addition, it supplies the subclasses
+`AsyncIOEventEmitter`, `TwistedEventEmitter` and `ExecutorEventEmitter`
 for supporting async and threaded execution with asyncio, twisted, and
 concurrent.futures Executors respectively, as supported by the environment.
 
+# Example
 
-Example
--------
+```text
+In [1]: from pyee.base import EventEmitter
 
-::
+In [2]: ee = EventEmitter()
 
-    In [1]: from pyee.base import EventEmitter
+In [3]: @ee.on('event')
+   ...: def event_handler():
+   ...:     print('BANG BANG')
+   ...:
 
-    In [2]: ee = EventEmitter()
+In [4]: ee.emit('event')
+BANG BANG
 
-    In [3]: @ee.on('event')
-       ...: def event_handler():
-       ...:     print('BANG BANG')
-       ...:
-
-    In [4]: ee.emit('event')
-    BANG BANG
-
-    In [5]:
+In [5]:
+```
 
 """
 
