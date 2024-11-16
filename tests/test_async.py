1330dd620ca284067a3cd4b8d1eb0efeb423c73c
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-16 10:58:09 -0900

    Tests for asyncio ee.cancel, ee.wait_for_complete, ee.complete

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-from asyncio import Future, wait_for
+from asyncio import Future, sleep, wait_for
 
 import pytest
 import pytest_asyncio.plugin  # noqa
@@ -24,7 +24,7 @@ class PyeeTestError(Exception):
 
 
 @pytest.mark.asyncio
-async def test_asyncio_emit(event_loop):
+async def test_asyncio_emit(event_loop) -> None:
     """Test that AsyncIOEventEmitter can handle wrapping
     coroutines
     """
@@ -44,11 +44,11 @@ async def test_asyncio_emit(event_loop):
     assert result is True
 
     await asyncio.sleep(0)
-    assert len(ee._waiting) == 0
+    assert ee.complete
 
 
 @pytest.mark.asyncio
-async def test_asyncio_once_emit(event_loop):
+async def test_asyncio_once_emit(event_loop) -> None:
     """Test that AsyncIOEventEmitter also wrap coroutines when
     using once
     """
@@ -69,7 +69,7 @@ async def test_asyncio_once_emit(event_loop):
 
 
 @pytest.mark.asyncio
-async def test_asyncio_error(event_loop):
+async def test_asyncio_error(event_loop) -> None:
     """Test that AsyncIOEventEmitter can handle errors when
     wrapping coroutines
     """
@@ -93,8 +93,8 @@ async def test_asyncio_error(event_loop):
 
 
 @pytest.mark.asyncio
-async def test_asyncio_cancellation(event_loop):
-    """Test that AsyncIOEventEmitter can handle Future cancellations"""
+async def test_asyncio_future_canceled(event_loop) -> None:
+    """Test that AsyncIOEventEmitter can handle canceled Futures"""
 
     cancel_me = Future(loop=event_loop)
     should_not_call = Future(loop=event_loop)
@@ -120,7 +120,62 @@ async def test_asyncio_cancellation(event_loop):
 
 
 @pytest.mark.asyncio
-async def test_sync_error(event_loop):
+async def test_asyncio_event_emitter_canceled(event_loop) -> None:
+    """Test that all running handlers in AsyncIOEventEmitter can be canceled"""
+
+    ee = AsyncIOEventEmitter(loop=event_loop)
+
+    should_not_call = Future(loop=event_loop)
+
+    @ee.on("event")
+    async def event_handler():
+        await sleep(1)
+        should_not_call.set_result(True)
+
+    ee.emit("event")
+
+    await sleep(0)
+
+    # event_handler should still be running
+    assert not ee.complete
+
+    # cancel all pending tasks, including event_handler
+    ee.cancel()
+
+    await sleep(0)
+
+    # event_handler should be canceled
+    assert ee.complete
+
+
+@pytest.mark.asyncio
+async def test_asyncio_wait_for_complete(event_loop) -> None:
+    """Test waiting for all pending tasks in an AsyncIOEventEmitter to
+    complete
+    """
+
+    ee = AsyncIOEventEmitter(loop=event_loop)
+
+    @ee.on("event")
+    async def event_handler():
+        await sleep(0.1)
+
+    ee.emit("event")
+
+    await sleep(0)
+
+    # event_handler should still be running
+    assert not ee.complete
+
+    # wait for event_handler to complete execution
+    await ee.wait_for_complete()
+
+    # event_handler should have finished execution
+    assert ee.complete
+
+
+@pytest.mark.asyncio
+async def test_sync_error(event_loop) -> None:
     """Test that regular functions have the same error handling as coroutines"""
     ee = AsyncIOEventEmitter(loop=event_loop)
 
@@ -141,7 +196,7 @@ async def test_sync_error(event_loop):
     assert isinstance(result, PyeeTestError)
 
 
-def test_twisted_emit():
+def test_twisted_emit() -> None:
     """Test that TwistedEventEmitter can handle wrapping
     coroutines
     """
@@ -159,7 +214,7 @@ def test_twisted_emit():
     should_call.assert_called_once()
 
 
-def test_twisted_once():
+def test_twisted_once() -> None:
     """Test that TwistedEventEmitter also wraps coroutines for
     once
     """
@@ -177,7 +232,7 @@ def test_twisted_once():
     should_call.assert_called_once()
 
 
-def test_twisted_error():
+def test_twisted_error() -> None:
     """Test that TwistedEventEmitters handle Failures when wrapping coroutines."""
     ee = TwistedEventEmitter()
 
