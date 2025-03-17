c5a44901cbcfcd6a4ac287f176b234d62a13b8d5
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-17 10:14:01 -0800

    Add back 3.8, 3.9 and 3.10 support
    This reverts commit a5561278bbcb05583c80d61be69aa52273e5dc32.

--- pyee/trio.py
+++ pyee/trio.py
@@ -7,9 +7,9 @@ from typing import (
     AsyncGenerator,
     Awaitable,
     Callable,
+    cast,
     Dict,
     Optional,
-    Self,
     Tuple,
     Type,
 )
@@ -17,6 +17,8 @@ from typing import (
 import trio
 
 from pyee.base import EventEmitter, PyeeError
+
+Self = Any
 
 __all__ = ["TrioEventEmitter"]
 
@@ -124,7 +126,7 @@ class TrioEventEmitter(EventEmitter):
         self._context: Optional[AbstractAsyncContextManager["TrioEventEmitter"]] = (
             self.context()
         )
-        return await self._context.__aenter__()
+        return await cast(Any, self._context).__aenter__()
 
     async def __aexit__(
         self: Self,
